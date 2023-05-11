29d54177b368ad2b535d0c63ca23595c6075ab13
Ajith <ajithkumar000@gmail.com>
2023-05-11 12:39:50 +0530

    Commit

---
+++ src/Components/AppRoutes/index.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Customers from "../../Pages/Customers";
+import Dashboard from "../../Pages/Dashbaord";
+import Products from "../../Pages/Products";
+import Orders from "../../Pages/Orders";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/products" element={<Products />}></Route>
+      <Route path="/orders" element={<Orders />}></Route>
+      <Route path="/customers" element={<Customers />}></Route>
+    </Routes>
+  );
+}
+export default AppRoutes;
---
+++ src/Pages/Dashbaord/index.js
@@ -0,0 +1,236 @@
+import {
+  DollarCircleOutlined,
+  ShoppingCartOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Card, Space, Statistic, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import {
+  getCustomers,
+  getInventory,
+  getOrders,
+  getProducts,
+  getRevenue,
+} from "../../API";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function Dashboard() {
+  const [orders, setOrders] = useState(0);
+  const [products, setproducts] = useState(0);
+  const [customers, setCustomers] = useState(0);
+  const [revenue, setRevenue] = useState(0);
+
+  useEffect(() => {
+    getOrders().then((res) => {
+      setOrders(res.total);
+      setRevenue(res.discountedTotal);
+    });
+    getProducts().then((res) => {
+      setproducts(res.total);
+    });
+    getCustomers().then((res) => {
+      setCustomers(res.total);
+    });
+  }, []);
+
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title level={4}>Dashboard</Typography.Title>
+      <Space direction="horizontal">
+        <DashboardCard
+          icon={
+            <ShoppingCartOutlined
+              style={{
+                color: "green",
+                backgroundColor: "rgba(0,255,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+                width: "50px",
+              }}
+            />
+          }
+          title={"Orders"}
+          value={orders}
+        />
+        <DashboardCard
+          icon={
+            <ShoppingOutlined
+              style={{
+                color: "blue",
+                backgroundColor: "rgba(0,0,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+                width: "50px",
+              }}
+            />
+          }
+          title={"Products"}
+          value={products}
+        />
+        <DashboardCard
+          icon={
+            <UserOutlined
+              style={{
+                color: "black",
+                backgroundColor: "#6F8FAF",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+                width: "50px",
+              }}
+            />
+          }
+          title={"Customer"}
+          value={customers}
+        />
+        <DashboardCard
+          icon={
+            <DollarCircleOutlined
+              style={{
+                color: "red",
+                backgroundColor: "rgba(255,0,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+                width: "50px",
+              }}
+            />
+          }
+          title={"Data"}
+          value={revenue}
+        />
+      </Space>
+      <Space>
+        <RecentOrders />
+        <DashboardChart />
+      </Space>
+    </Space>
+  );
+}
+
+function DashboardCard({ title, value, icon }) {
+  return (
+    <Card>
+      <Space direction="horizontal">
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
+function RecentOrders() {
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getOrders().then((res) => {
+      setDataSource(res.products.splice(0, 3));
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <Typography.Text>Recent Orders</Typography.Text>
+      <Table
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+          },
+          {
+            title: "Price",
+            dataIndex: "discountedPrice",
+          },
+        ]}
+        loading={loading}
+        dataSource={dataSource}
+        pagination={false}
+        width="100%"
+      ></Table>
+    </>
+  );
+}
+
+function DashboardChart() {
+  const [reveneuData, setReveneuData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    getRevenue().then((res) => {
+      const labels = res.carts.map((cart) => {
+        return `User-${cart.userId}`;
+      });
+      const data = res.carts.map((cart) => {
+        return cart.discountedTotal;
+      });
+
+      const dataSource = {
+        labels,
+        datasets: [
+          {
+            label: "Data",
+            data: data,
+            backgroundColor: "#0096FF",
+          },
+        ],
+      };
+
+      setReveneuData(dataSource);
+    });
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Order Data",
+      },
+    },
+  };
+
+  return (
+    <Card style={{ width: 900, height: 450 }}>
+      <Bar
+        style={{ width: 900, height: 400 }}
+        options={options}
+        data={reveneuData}
+      />
+    </Card>
+  );
+}
+export default Dashboard;
